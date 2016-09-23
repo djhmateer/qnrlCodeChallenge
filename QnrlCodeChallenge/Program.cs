b3da2194149f19a4ecbbc4ce4666985db655fa5e
Dave Mateer <davemateer@gmail.com>
2016-09-23 15:59:53 +0100

    Auto commit at 23/09/2016 15:59

--- QnrlCodeChallenge/Program.cs
+++ QnrlCodeChallenge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace QnrlCodeChallenge
 {
@@ -6,24 +8,104 @@ namespace QnrlCodeChallenge
     {
         static void Main()
         {
-            // Use a enum to represent cards
-            Card a = Card.Club;
+            var deck = new Deck();
+            deck.Shuffle();
 
-            // 5 random cards
-            var rnd = new Random();
-            for (var i = 0; i < 5; i++)
+            foreach (var card in deck.Cards)
             {
-                var card = (Card)rnd.Next(0, 4);
                 Console.WriteLine(card);
             }
+
+            Console.WriteLine("total cards: " + deck.Cards.Count);
         }
     }
 
-    enum Card
+    class Deck
     {
-        Heart,
-        Diamond,
-        Club,
-        Spade
+        // Field (was called List in spec.. prefer Cards)
+        public IList<Card> Cards;
+        // Property
+        public int NumberOfCards { get; set; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            // Populate 52 cards in order
+            for (int i = 0; i < 4; i++)
+            {
+                // Get suit based on int value of enum
+                Suit suit = (Suit)i;
+                for (int j = 1; j < 14; j++)
+                {
+                    Rank rank = (Rank)j;
+                    Cards.Add(new Card { Suit = suit, Rank = rank });
+                }
+            }
+        }
+        
+        // can see that they want encapsulation
+        // will go down that road but next stop would be a more data structure style of OO
+        // with more testable functions eg 
+        // public IList<Card> Shuffle(IList<Card> input)
+        public void Shuffle()
+        {
+            var unshuffled = new List<Card>();
+            var shuffled = new List<Card>();
+            unshuffled = Cards.ToList();
+
+            int count = unshuffled.Count;
+            var rnd = new Random();
+            for (var i = 0; i < count; i++)
+            {
+                // get a random card from the unshuffled list
+                var index = rnd.Next(0, unshuffled.Count-1);
+                var card = unshuffled[index];
+
+                // put onto new shuffled list
+                shuffled.Add(card);
+
+                // delete from old unshuffled list
+                unshuffled.RemoveAt(index);
+            }
+
+            Cards = shuffled;
+        }
+    }
+
+    // struct ROT - small, use when immutable once created
+    struct Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+
+        public override string ToString()
+        {
+            return Rank + " of " + Suit;
+        }
+    }
+
+    enum Suit
+    {
+        Hearts = 0,
+        Diamonds = 1,
+        Clubs = 2,
+        Spades = 3
+    }
+
+    enum Rank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
     }
 }
