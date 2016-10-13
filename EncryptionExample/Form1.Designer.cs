c8f3c0a12f0561a3c5e556c5a254123467f42c22
Dave Mateer <davemateer@gmail.com>
2016-10-13 16:58:45 +0100

    Auto commit at 13/10/2016 16:58

--- EncryptionExample/Form1.Designer.cs
+++ EncryptionExample/Form1.Designer.cs
@@ -28,24 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSendMessage = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnEncrypt = new System.Windows.Forms.Button();
             this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.btnSendFileCaesar = new System.Windows.Forms.Button();
+            this.btnReadDecyrptCaesarWrite = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSendMessage
             // 
-            this.button1.Location = new System.Drawing.Point(119, 159);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Send";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSendMessage.Location = new System.Drawing.Point(80, 159);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(114, 24);
+            this.btnSendMessage.TabIndex = 0;
+            this.btnSendMessage.Text = "SendMessage";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
             // 
             // textBox1
             // 
@@ -85,7 +88,7 @@
             // 
             this.btnEncrypt.Location = new System.Drawing.Point(25, 198);
             this.btnEncrypt.Name = "btnEncrypt";
-            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.Size = new System.Drawing.Size(91, 31);
             this.btnEncrypt.TabIndex = 5;
             this.btnEncrypt.Text = "Encrypt";
             this.btnEncrypt.UseVisualStyleBackColor = true;
@@ -95,24 +98,57 @@
             // 
             this.btnDecrypt.Location = new System.Drawing.Point(282, 198);
             this.btnDecrypt.Name = "btnDecrypt";
-            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.Size = new System.Drawing.Size(92, 31);
             this.btnDecrypt.TabIndex = 6;
             this.btnDecrypt.Text = "Decrypt";
             this.btnDecrypt.UseVisualStyleBackColor = true;
             this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(80, 268);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(133, 24);
+            this.btnSendFile.TabIndex = 7;
+            this.btnSendFile.Text = "SendFileNoCrypto";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFileNoCrypto_Click);
+            // 
+            // btnSendFileCaesar
+            // 
+            this.btnSendFileCaesar.Location = new System.Drawing.Point(25, 298);
+            this.btnSendFileCaesar.Name = "btnSendFileCaesar";
+            this.btnSendFileCaesar.Size = new System.Drawing.Size(188, 24);
+            this.btnSendFileCaesar.TabIndex = 8;
+            this.btnSendFileCaesar.Text = "ReadEncryptCaesarWrite";
+            this.btnSendFileCaesar.UseVisualStyleBackColor = true;
+            this.btnSendFileCaesar.Click += new System.EventHandler(this.btnSendFileCaesar_Click);
+            // 
+            // btnReadDecyrptCaesarWrite
+            // 
+            this.btnReadDecyrptCaesarWrite.Location = new System.Drawing.Point(282, 291);
+            this.btnReadDecyrptCaesarWrite.Name = "btnReadDecyrptCaesarWrite";
+            this.btnReadDecyrptCaesarWrite.Size = new System.Drawing.Size(185, 31);
+            this.btnReadDecyrptCaesarWrite.TabIndex = 9;
+            this.btnReadDecyrptCaesarWrite.Text = "ReadDecryptCaesarWrite";
+            this.btnReadDecyrptCaesarWrite.UseVisualStyleBackColor = true;
+            this.btnReadDecyrptCaesarWrite.Click += new System.EventHandler(this.btnReadDecyrptCaesarWrite_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 355);
+            this.Controls.Add(this.btnReadDecyrptCaesarWrite);
+            this.Controls.Add(this.btnSendFileCaesar);
+            this.Controls.Add(this.btnSendFile);
             this.Controls.Add(this.btnDecrypt);
             this.Controls.Add(this.btnEncrypt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSendMessage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -122,13 +158,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSendMessage;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnEncrypt;
         private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Button btnSendFileCaesar;
+        private System.Windows.Forms.Button btnReadDecyrptCaesarWrite;
     }
 }
 
--- EncryptionExample/Form1.cs
+++ EncryptionExample/Form1.cs
@@ -12,15 +12,15 @@ namespace EncryptionExample
             textBox1.Text = "Attack at dawn";
         }
 
-        void button1_Click(object sender, EventArgs e)
+        void btnSendMessage_Click(object sender, EventArgs e)
         {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
             writer.Write(textBox1.Text);
             writer.Flush();
-            stream.Position = 0;
+            ms.Position = 0;
 
-            var sr = new StreamReader(stream);
+            var sr = new StreamReader(ms);
             var x = sr.ReadToEnd();
             textBox2.Text = x;
         }
@@ -52,6 +52,100 @@ namespace EncryptionExample
                 clearText += (char)x;
             }
             textBox2.Text = clearText;
+        }
+
+        void btnSendFileNoCrypto_Click(object sender, EventArgs e)
+        {
+            // read file to a memoryStream 
+            var ms = new MemoryStream();
+            using (var file = new FileStream(@"c:\temp\user1.txt", FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+                ms.Write(bytes, 0, (int)file.Length);
+            }
+            ms.Position = 0;
+            // no encryption here
+
+            // write file from memoryStream
+            using (var file = new FileStream(@"c:\temp\user2.txt", FileMode.Create, FileAccess.Write))
+            {
+                var bytes = new byte[ms.Length];
+                ms.Read(bytes, 0, (int)ms.Length);
+                file.Write(bytes, 0, bytes.Length);
+                ms.Close();
+            }
+        }
+
+        void btnSendFileCaesar_Click(object sender, EventArgs e)
+        {
+            // read file to a memoryStream 
+            var ms = new MemoryStream();
+            using (var file = new FileStream(@"c:\temp\user1.txt", FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+                ms.Write(bytes, 0, (int)file.Length);
+            }
+            ms.Position = 0;
+
+            // write file from memoryStream
+            using (var file = new FileStream(@"c:\temp\user2.txt", FileMode.Create, FileAccess.Write))
+            {
+                var bytes = new byte[ms.Length];
+                ms.Read(bytes, 0, (int)ms.Length);
+
+                //// Caesar shift encryption on memory stream bytes
+                var cypherBytes = new byte[ms.Length];
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    byte b = bytes[i];
+                    // if add 1 to a char, it goes to next letter
+                    // eg A is 65, B is 66
+                    // implicit conversion to int and add 1
+                    int x = b + 1;
+                    // convert int to byte
+                    cypherBytes[i] = (byte)x;
+                }
+                //file.Write(bytes, 0, bytes.Length);
+                file.Write(cypherBytes, 0, cypherBytes.Length);
+                ms.Close();
+            }
+        }
+
+        void btnReadDecyrptCaesarWrite_Click(object sender, EventArgs e)
+        {
+            // read file 
+            var ms = new MemoryStream();
+            using (var file = new FileStream(@"c:\temp\user2.txt", FileMode.Open, FileAccess.Read))
+            {
+                var cypherBytes = new byte[file.Length];
+                file.Read(cypherBytes, 0, (int)file.Length);
+                ms.Write(cypherBytes, 0, (int)file.Length);
+
+                // decrypt
+                var clearBytes = new byte[ms.Length];
+                for (int i = 0; i < cypherBytes.Length; i++)
+                {
+                    byte b = cypherBytes[i];
+                    int x = b - 1;
+                    clearBytes[i] = (byte)x;
+                }
+                ms.Position = 0;
+
+                // write clearBytes back to memory stream
+                ms.Write(clearBytes, 0, (int)file.Length);
+            }
+            ms.Position = 0;
+
+            // write clearBytes from memoryStream to file
+            using (var file = new FileStream(@"c:\temp\user2.txt", FileMode.Create, FileAccess.Write))
+            {
+                var bytes = new byte[ms.Length];
+                ms.Read(bytes, 0, (int)ms.Length);
+                file.Write(bytes, 0, bytes.Length);
+                ms.Close();
+            }
         }
     }
 }
